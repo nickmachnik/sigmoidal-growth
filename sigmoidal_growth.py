69ba49c0d6191b421d8fc1723d8441e91aaf018d
Nick Machnik <nick.machnik@gmail.com>
2020-04-14 23:10:14 +0200

    docs

--- sigmoidal_growth.py
+++ sigmoidal_growth.py
@@ -4,35 +4,26 @@ import numpy as np
 from scipy.optimize import curve_fit
 from scipy.misc import derivative
 
-def _center_data(data):
-    """Summary
-    
-    Args:
-        data (TYPE): Description
-    
-    Returns:
-        TYPE: Description
-    """
-    half_max = (data[-1] + np.min(data)) / 2
-    for index, num in enumerate(data):
-        if num > half_max:
-            return np.arange(-index, -index + len(data))
 
-def _truncate_data(data):
-    """
-    Truncate data such that the halfmax value is the median.
+def _center_data(data: array_like) -> array_like:
+    """Center the input data around the median value between min and max.
     
     Args:
-        data (TYPE): Description
+        data (array_like): Data with approximately sigmoidal shape.
     
     Returns:
-        TYPE: Description
+        array_like: x-values of the centered data, centered around 0.
+        array_like: Centered data.
     """
+    # truncate at maximum
     data = data[:np.argmax(data) + 1]
-    x = center_data(data)
-    return x[-x[-1] * 2:], data[-x[-1] * 2:]
+    half_max = (data[-1] + np.min(data)) / 2
+    len_upper_half = len(data) - (np.argmax(data > half_max))
+    x = np.arange(-len_upper_half, len_upper_half)
+    return x, data[-(len_upper_half*2):]
 
-def max_growth_rate(y_data):
+
+def max_growth_rate(y_data: array_like) -> (float, float):
     """Determine maximal slope in data by fitting a sigmoid function.
     
     Args:
@@ -46,7 +37,7 @@ def max_growth_rate(y_data):
         y = A + ((L - A) / (1 + np.exp(-b*(x-a))))
         return y
     
-    X, y_data = truncate_data(y_data)
+    X, y_data = _center_data(y_data)
     A = np.min(y_data)
     L = np.max(y_data)
     a = 0
